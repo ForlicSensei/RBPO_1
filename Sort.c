9e46f759ba266a2eaa789a1f03f95094d333611b
Anton <62097689+ForlicSensei@users.noreply.github.com>
2021-10-08 15:24:06 +0300

    Final

---
+++ Sort.c
@@ -0,0 +1,69 @@
+#include "lib.h"
+
+void Sort(struct Person* persons, int count, enum SortType sortType) {
+    int tmp, k; // готовим начальный шаг
+    int s = count;
+    if (sortType == ASC) {
+        while (count > 1) {
+            s /= 1.247f;
+            if (s < 1) s = 1; // 0 быть не может, присвоим 1
+            k = 0; // нет перестановок
+            for (int i = 0; i + s < count; i++) { // двигаемся, пока сравниваемый элемент (на s от текущего) в массиве
+                if (CompareString(persons[i].firstName, persons[i + s].firstName) == -1) {
+                    struct Person tmp = persons[i];
+                    persons[i] = persons[i + s];
+                    persons[i + s] = tmp;
+                } else if (CompareString(persons[i].firstName, persons[i + s].firstName) == 0)
+                    if (CompareString(persons[i].secondName, persons[i + s].secondName) == -1) {
+                        struct Person tmp = persons[i];
+                        persons[i] = persons[i + s];
+                        persons[i + s] = tmp;
+                    } else if (CompareString(persons[i].secondName, persons[i + s].secondName) == 0)
+                        if (CompareString(persons[i].thirdName, persons[i + s].thirdName) == -1) {
+                            struct Person tmp = persons[i];
+                            persons[i] = persons[i + s];
+                            persons[i + s] = tmp;
+                        } else if (CompareString(persons[i].thirdName, persons[i + s].thirdName) == 0)
+                            if (persons[i].birthYear <= persons[i + s].birthYear) {
+                                struct Person tmp = persons[i];
+                                persons[i] = persons[i + s];
+                                persons[i + s] = tmp;
+                            }
+            }
+            if (s == 1) // шаг 1, как в обычном пузырьке. Включаем контроль
+                count = k + 1;
+        }
+
+    }
+    else {
+        while (count > 1) {
+            s /= 1.247f;
+            if (s < 1) s = 1; // 0 быть не может, присвоим 1
+            k = 0; // нет перестановок
+            for (int i = 0; i + s < count; i++) { // двигаемся, пока сравниваемый элемент (на s от текущего) в массиве
+                if (CompareString(persons[i].firstName, persons[i + s].firstName) == 1) {
+                    struct Person tmp = persons[i];
+                    persons[i] = persons[i + s];
+                    persons[i + s] = tmp;
+                } else if (CompareString(persons[i].firstName, persons[i + s].firstName) == 0)
+                    if (CompareString(persons[i].secondName, persons[i + s].secondName) == 1) {
+                        struct Person tmp = persons[i];
+                        persons[i] = persons[i + s];
+                        persons[i + s] = tmp;
+                    } else if (CompareString(persons[i].secondName, persons[i + s].secondName) == 0)
+                        if (CompareString(persons[i].thirdName, persons[i + s].thirdName) == 1) {
+                            struct Person tmp = persons[i];
+                            persons[i] = persons[i + s];
+                            persons[i + s] = tmp;
+                        } else if (CompareString(persons[i].thirdName, persons[i + s].thirdName) == 0)
+                            if (persons[i].birthYear >= persons[i + s].birthYear) {
+                                struct Person tmp = persons[i];
+                                persons[i] = persons[i + s];
+                                persons[i + s] = tmp;
+                            }
+            }
+            if (s == 1) // шаг 1, как в обычном пузырьке. Включаем контроль
+                count = k + 1;
+        }
+    }
+}
---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/untitled1/CompareStrings.c" "D:/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/CompareStrings.c.obj"
+  "D:/untitled1/GetPersons.c" "D:/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/GetPersons.c.obj"
+  "D:/untitled1/PrintAllPerson.c" "D:/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/PrintAllPerson.c.obj"
+  "D:/untitled1/Sort.c" "D:/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/Sort.c.obj"
+  "D:/untitled1/main.c" "D:/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
